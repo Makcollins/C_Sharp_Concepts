9cd3f71889dd8bed1761ca3e3fa7245290b58a20
Collins Makui <collinsm@syncfusion.com>
2025-10-02 17:10:36 +0300

    filters

--- ADVANCE/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ ADVANCE/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d30e7c55dcaeaed2bfd8366a2d9e8d1edaf15a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c28541341b7236144122a556f11d250bcc7c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
